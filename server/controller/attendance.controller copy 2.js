4621cc4a032130a5fdb734482106938ea501ac23
Ebel Ofori <61881003+Uncle-K-1@users.noreply.github.com>
2022-11-25 14:40:20 +0000

    c

---
+++ server/controller/attendance.controller copy 2.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+//const Animal = db.animals;
+
+// Create and Save a new Animal
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Animals from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Animal with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Animal by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Animal with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Animal from the database.
+exports.deleteAll = (req, res) => {
+
+};
